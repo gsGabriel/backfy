ec8b603de413ad2030a03e28cafe3bd8f9ffa24a
gsGabriel <gs.gabrielcfa@outlook.com>
2019-02-24 13:01:59 -0300

    Configuração da infra dos projetos de testes

---
+++ test/Sales/Backfy.Sales.Command.Tests/Infra/ConfigureTestFramework.cs
@@ -0,0 +1,21 @@
+﻿using Autofac;
+using Xunit;
+using Xunit.Abstractions;
+using Xunit.Frameworks.Autofac;
+
+[assembly: TestFramework("Backfy.Sales.Command.Tests.Infra.ConfigureTestFramework", "Backfy.Sales.Command.Tests")]
+namespace Backfy.Sales.Command.Tests.Infra
+{
+    public class ConfigureTestFramework : AutofacTestFramework
+    {
+        public ConfigureTestFramework(IMessageSink diagnosticMessageSink)
+            : base(diagnosticMessageSink)
+        {
+        }
+
+        protected override void ConfigureContainer(ContainerBuilder builder)
+        {
+            
+        }
+    }
+}
---
+++ test/Sales/Backfy.Sales.Repository.Tests/Infra/ConfigureTestFramework.cs
@@ -0,0 +1,21 @@
+﻿using Autofac;
+using Xunit;
+using Xunit.Abstractions;
+using Xunit.Frameworks.Autofac;
+
+[assembly: TestFramework("Backfy.Sales.Repository.Tests.Infra.ConfigureTestFramework", "Backfy.Sales.Repository.Tests")]
+namespace Backfy.Sales.Repository.Tests.Infra
+{
+    public class ConfigureTestFramework : AutofacTestFramework
+    {
+        public ConfigureTestFramework(IMessageSink diagnosticMessageSink)
+            : base(diagnosticMessageSink)
+        {
+        }
+
+        protected override void ConfigureContainer(ContainerBuilder builder)
+        {
+            
+        }
+    }
+}
